3ecf7f62785e83ff2493e987fb6d33708a2cb1bb
Wil Gieseler <wil@wilgieseler.com>
2013-11-29 22:16:14 -0800

    redirect to new domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :redirect_if_old
+
+  protected
+
+  def redirect_if_old
+    if request.host == 'headline-generator.herokuapp.com'
+      redirect_to "http://www.headlinesmasher.com#{request.fullpath}", :status => :moved_permanently
+    end
+  end
+
 end
