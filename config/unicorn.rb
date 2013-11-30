eaa03a545173220c0d92df533d677ad2580c4de7
Wil Gieseler <wil@wilgieseler.com>
2013-11-30 11:21:20 -0800

    unicorn connection pool

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,6 +17,10 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']            = ENV['DB_POOL'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
