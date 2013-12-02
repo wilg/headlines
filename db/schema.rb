7b1eac35fcb5448e9eddee273b183bd18128270c
Wil Gieseler <wil@wilgieseler.com>
2013-12-01 20:40:09 -0800

    don't use activerecord session store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202041835) do
+ActiveRecord::Schema.define(version: 20131202043925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,16 +26,6 @@ ActiveRecord::Schema.define(version: 20131202041835) do
     t.integer  "creator_id"
     t.integer  "vote_count"
   end
-
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "login",               default: "", null: false
