2f6d26661eef524232ba576eb88c086c7f868b3b
Wil Gieseler <wil@wilgieseler.com>
2013-12-29 23:08:13 -0800

    correct order for fragments

--- app/models/source_headline_fragment.rb
+++ app/models/source_headline_fragment.rb
@@ -3,6 +3,8 @@ class SourceHeadlineFragment < ActiveRecord::Base
   belongs_to :source_headline
   belongs_to :headline
 
+  scope :ordered, -> { order("index asc") }
+
   def fragment
     source_headline.name[source_headline_start...source_headline_end]
   end
