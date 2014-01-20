fd780e1683f3c853653e3e06e34fa983d6c4cccc
Wil Gieseler <wil@wilgieseler.com>
2014-01-19 20:13:16 -0800

    better generator source exemptions

--- app/helpers/generator_helper.rb
+++ app/helpers/generator_helper.rb
@@ -1,2 +1,8 @@
 module GeneratorHelper
+
+  def generator_sources
+    rejected_ids = [:r_politics, :r_funny]
+    HeadlineSources::Source.all.reject{|s| rejected_ids.include?(s.id) }.sort_by{|s| s.category}.group_by{|s| s.category }
+  end
+
 end
