3f646615b9d60c81de2cb4558f75a65e5f4eaa57
Wil Gieseler <wil@wilgieseler.com>
2014-01-24 11:43:35 -0800

    sort headline source page

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -2,7 +2,7 @@ class SourcesController < ApplicationController
 
   def show
     @source = HeadlineSource.find(params[:id])
-    @headlines = @source.headlines.top.paginate(:page => params[:page], :per_page => 40)
+    @headlines = default_pagination headlines_sorted_by_params @source.headlines.top
   end
 
 end
--- app/models/headline.rb
+++ app/models/headline.rb
@@ -3,7 +3,7 @@ require 'digest/sha1'
 class Headline < ActiveRecord::Base
 
   scope :top, -> { order("headlines.vote_count desc, headlines.created_at desc") }
-  scope :hot, -> { order("(headlines.vote_count / (extract(epoch from now()) - extract(epoch from headlines.created_at))) desc").where("created_at < ?", 20.minutes.ago).where("headlines.vote_count > 1 AND headlines.vote_count < 50") }
+  scope :hot, -> { order("(headlines.vote_count / (extract(epoch from now()) - extract(epoch from headlines.created_at))) desc").where("headlines.created_at < ?", 20.minutes.ago).where("headlines.vote_count > 1 AND headlines.vote_count < 50") }
   scope :today, -> { where("headlines.created_at > ?", 1.day.ago) }
   scope :this_week, -> { where("headlines.created_at > ?", 7.days.ago) }
   scope :yesterday, -> { where("headlines.created_at > ? AND headlines.created_at < ?", 2.days.ago, 1.day.ago) }
