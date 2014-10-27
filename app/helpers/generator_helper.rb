5343b7832705522cb5fd66212b26c8aaf4ac1469
Wil Gieseler <wil@wilgieseler.com>
2014-10-26 20:48:50 -0700

    reject dead

--- app/helpers/generator_helper.rb
+++ app/helpers/generator_helper.rb
@@ -1,8 +1,7 @@
 module GeneratorHelper
 
   def generator_sources
-    rejected_ids = [:r_politics, :r_funny]
-    HeadlineSources::Source.all.reject{|s| rejected_ids.include?(s.id) }.sort_by{|s| s.category}.group_by{|s| s.category }
+    HeadlineSources::Source.all.reject{|s| s.dead }.sort_by{|s| s.category}.group_by{|s| s.category }
   end
 
   def generate_url_like(headline)
