899d0c1319ba80baa2e5bf206d7442de00a55e54
Wil Gieseler <wil@wilgieseler.com>
2016-02-18 23:14:44 -0800

    rollbar js

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -54,4 +54,14 @@ Rollbar.configure do |config|
   # setup for Heroku. See:
   # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
   config.environment = ENV['ROLLBAR_ENV'] || Rails.env
+
+  config.js_enabled = true
+  config.js_options = {
+    accessToken: ENV['ROLLBAR_CLIENT_ACCESS_TOKEN'],
+    captureUncaught: true,
+    payload: {
+      environment: config.environment
+    }
+  }
+
 end
