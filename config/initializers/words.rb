9e688caf27808dbc0fc4a4848b51debf28b2f847
Wil Gieseler <wil@wilgieseler.com>
2018-08-24 11:59:01 -0700

    i didn't understand %w and neither did syntax highlighting

--- config/initializers/words.rb
+++ config/initializers/words.rb
@@ -1,6 +1,7 @@
+# these will be filtered *after* parameterize so - may be substituted for special characters
 DISALLOWED_WORDS = %w[
   rape
   nigger
   nigga
-  n-gg # the * will be hashed as -
+  n-gg
 ]
