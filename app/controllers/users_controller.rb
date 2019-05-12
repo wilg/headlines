5c377830e86ed7c4cd465ee14fb9898c31c41034
Wil Gieseler <wil@wilgieseler.com>
2019-05-12 12:40:38 -0700

    unknown username 404s

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_login(params[:id])
+    raise ActionController::RoutingError.new('Not Found') unless @user
   end
 
 end
